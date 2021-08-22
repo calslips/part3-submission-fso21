adf02e7dc2c00254bcd8428daa1ec0ffd117f523
Caleb Phillips <csphillips5@gmail.com>
2021-08-22 02:03:35 -0400

    implement DELETE req for single person

--- index.js
+++ index.js
@@ -28,8 +28,10 @@ let persons = [
 app.get('/info', (req, res) => {
   res.send(
     `
-    <p>Phonebook has info for ${persons.length} people<p>
-    <p>${new Date()}<p>
+    <div>
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>
+    </div>
     `
   );
 });
@@ -45,6 +47,13 @@ app.get('/api/persons/:id', (req, res) => {
   person
     ? res.json(person)
     : res.status(404).end();
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+
+  res.status(204).end();
 });
 
 const PORT = 3001;
