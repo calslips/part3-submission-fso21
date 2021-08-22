16ac3208a5cf815aeff6a0f2e2d260ef4332cab0
Caleb Phillips <csphillips5@gmail.com>
2021-08-22 00:58:25 -0400

    implement GET req for route info, res shows # of entries & timestamp 3.2

--- index.js
+++ index.js
@@ -25,6 +25,15 @@ let persons = [
   }
 ];
 
+app.get('/info', (req, res) => {
+  res.send(
+    `
+    <p>Phonebook has info for ${persons.length} people<p>
+    <p>${new Date()}<p>
+    `
+  );
+});
+
 app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
